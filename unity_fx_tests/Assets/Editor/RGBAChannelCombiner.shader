dbc723759bd7a4e885803c01f7a58014a9a96117
cptcaveman <wsndrs@gmail.com>
2017-11-29 12:31:21 -0800

    simple texture channel combiner tool in editor so i can rapidly make different compact effects textures

---
+++ unity_fx_tests/Assets/Editor/RGBAChannelCombiner.shader
@@ -0,0 +1,60 @@
+﻿Shader "Hidden/RGBAChannelCombiner"
+{
+	Properties
+	{
+		_MainTex ("Tex for R Channel", 2D) = "black" {}
+		_TexG("Tex for G Channel", 2D) = "black" {}
+		_TexB("Tex for B Channel", 2D) = "black" {}
+		_TexA("Tex for A Channel", 2D) = "black" {}
+	}
+	SubShader
+	{
+		// No culling or depth
+		Cull Off ZWrite Off ZTest Always
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv;
+				return o;
+			}
+			
+			sampler2D _MainTex;
+			sampler2D _TexG;
+			sampler2D _TexB;
+			sampler2D _TexA;
+
+			fixed4 frag (v2f i) : SV_Target
+			{
+				fixed r = tex2D(_MainTex, i.uv).r;
+				fixed g = tex2D(_TexG, i.uv).g;
+				fixed b = tex2D(_TexG, i.uv).b;
+				fixed a = tex2D(_TexG, i.uv).a;
+				
+				return fixed4(r, g, b, a);
+			}
+			ENDCG
+		}
+	}
+}
---
+++ unity_fx_tests/Assets/Editor/TextureCombinerEditorWindow.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class TextureCombinerWindow : EditorWindow
+{
+    private Material m_rgbaMat;
+    private Texture2D m_prevMadeTexture;
+    private const int m_textureSize = 64;
+    private const int m_labelHeight = 16;
+    private const int m_padding = 10;
+
+    Rect m_textureRect = new Rect();
+    Rect m_labelRect = new Rect();
+
+    Texture2D m_texR;
+    Texture2D m_texG;
+    Texture2D m_texB;
+    Texture2D m_texA;
+
+    [MenuItem("Tools/Texture Combiner")]
+    public static void CreateWindow()
+    {
+        TextureCombinerWindow window = GetWindow<TextureCombinerWindow>();
+        window.Show();
+    }
+
+    private void OnEnable()
+    {
+        m_rgbaMat = new Material(Shader.Find("Hidden/RGBAChannelCombiner"));
+        m_prevMadeTexture = null;
+    }
+
+    private void DrawTextureField(string label, ref Texture2D tex, float x, float y)
+    {
+        m_labelRect.x = x;
+        m_labelRect.y = y;
+        GUI.Label(m_labelRect, label);
+
+        m_textureRect.x = x;
+        m_textureRect.y = y + m_labelHeight;
+        tex = (Texture2D)EditorGUI.ObjectField(m_textureRect, tex, typeof(Texture2D), false);
+    }
+
+    private void OnGUI()
+    {
+        float padding = m_padding;
+        float y = padding;
+
+        // set up default dimensions and positions of rects
+        m_labelRect.y = y;
+        m_labelRect.height = m_labelHeight * 2;
+        m_labelRect.width = m_textureSize * 5;
+
+        EditorGUI.HelpBox(m_labelRect, "Please ensure the textures are of the same dimensions", MessageType.Info);
+
+        y += m_labelRect.height;
+
+        m_labelRect.height = m_labelHeight;
+        m_labelRect.width = m_textureRect.width = m_textureRect.height = m_textureSize;
+
+        DrawTextureField("R Channel:", ref m_texR, padding, y);
+        y += m_labelHeight + m_textureSize;
+
+        DrawTextureField("G Channel:", ref m_texG, padding, y);
+        y += m_labelHeight + m_textureSize;
+
+        DrawTextureField("B Channel:", ref m_texB, padding, y);
+        y += m_labelHeight + m_textureSize;
+
+        DrawTextureField("A Channel:", ref m_texA, padding, y);
+        y += m_labelHeight + m_textureSize;
+
+        m_labelRect.y = this.position.height - padding * 2;
+        m_labelRect.width *= 2;
+        
+        if (GUI.Button(m_labelRect, "Combine Textures"))
+        {
+            // Combine Textures
+            RenderTexture rt = new RenderTexture(m_texR.width, m_texR.height, 0, RenderTextureFormat.Default, RenderTextureReadWrite.Default);
+
+            m_rgbaMat.SetTexture("_TexG", m_texG);
+            m_rgbaMat.SetTexture("_TexB", m_texB);
+            m_rgbaMat.SetTexture("_TexA", m_texA);
+
+            Graphics.Blit(m_texR, rt, m_rgbaMat);
+
+            RenderTexture.active = rt;
+
+            m_prevMadeTexture = new Texture2D(m_texR.width, m_texR.height);
+            m_prevMadeTexture.ReadPixels(new Rect(0, 0, m_prevMadeTexture.width, m_prevMadeTexture.height), 0, 0);
+            m_prevMadeTexture.Apply();
+            byte[] bytes = m_prevMadeTexture.EncodeToPNG();
+
+            using (System.IO.FileStream fs = new System.IO.FileStream(Application.dataPath + "/New RGBA Channel Texture.png", System.IO.FileMode.Create))
+            {
+                fs.Write(bytes, 0, bytes.Length);
+            }
+
+            AssetDatabase.Refresh();
+        }
+
+        if(m_prevMadeTexture)
+        {
+            // draw texture to the right
+        }
+    }
+
+    private void OnDisable()
+    {
+        Debug.Log("asdfasdf");
+    }
+}
